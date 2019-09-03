d760b3338e4cea13d778327eb49956921b170e78
Antony Blyakher <ablyakhe@uncc.edu>
2019-09-03 20:29:41 +0000

    RubyBasics1 - Part 3

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -2,29 +2,43 @@
 # Part I
 def sum arr
   sum = 0
-    arr.each{
-      |i| 
+    arr.each do |i| 
       sum=sum+i 
-    }
+    end
   return sum
 end
 
 # Part II
 def max_2_sum arr
-  arr=arr.sort
+  arr=arr.sort #sorts array
   max_2_sum = 0
-    if arr.empty?
+    if arr.empty? #if array is empty, sum is 0
       max_2_sum = 0
-    else if arr.count==1
+    elsif arr.count==1 #if array is single element, sum is the one element
       max_2_sum = arr[0]
     else
       max_2_sum = (arr[-1]+arr[-2])
     end
-  end
   return max_2_sum
 end
 
+
 # Part III
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  #checks if single element array
+  if arr.count==1
+    return false
+  end
+  
+  #nested loop through array, to check the sum of each element with all the other elements
+  arr.each do |j|
+    arr.each do |k|
+      if j!=k #to avoid element summing with itself
+        if (j+k)==n
+          return true
+        end
+      end
+    end
+  end  
+  return false
 end
