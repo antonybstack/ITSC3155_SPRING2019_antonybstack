25e37c8fc6e11bfde2d19288c12cab77711ba476
Antony Blyakher <ablyakhe@uncc.edu>
2019-09-03 19:09:43 +0000

    RubyBasics1 - Part 2

--- RubyBasics1/lib/rubybasics1.rb
+++ RubyBasics1/lib/rubybasics1.rb
@@ -1,14 +1,27 @@
 # Lab 1
 # Part I
 def sum arr
-    sum = 0
-    arr.each{|element| sum=sum+element }
-    return sum
+  sum = 0
+    arr.each{
+      |i| 
+      sum=sum+i 
+    }
+  return sum
 end
 
 # Part II
 def max_2_sum arr
-  # YOUR CODE HERE
+  arr=arr.sort
+  max_2_sum = 0
+    if arr.empty?
+      max_2_sum = 0
+    else if arr.count==1
+      max_2_sum = arr[0]
+    else
+      max_2_sum = (arr[-1]+arr[-2])
+    end
+  end
+  return max_2_sum
 end
 
 # Part III
