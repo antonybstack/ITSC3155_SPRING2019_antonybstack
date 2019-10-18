015fba403720aad448b2a571ca23ba6bf5b69fb3
Antony Blyakher <ablyakhe@uncc.edu>
2019-10-18 22:38:41 +0000

    Add Edit/Update functionality

--- blog/app/models/article.rb
+++ blog/app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
     validates :title, presence: true, length: { minimum: 5 }
-    validates :title, length: { maximum: 10 }
+    validates :title, length: { maximum: 30 }
 end
