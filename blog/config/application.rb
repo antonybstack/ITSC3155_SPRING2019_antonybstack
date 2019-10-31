f8060ec1140a58c6fc7077cf0bdf0136e26247b2
antonybstack <ablyakhe@uncc.edu>
2019-10-31 04:37:40 +0000

    fixed server issues

--- blog/config/application.rb
+++ blog/config/application.rb
@@ -21,5 +21,6 @@ module Blog
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.web_console.whitelisted_ips = '152.15.112.71'
   end
 end
